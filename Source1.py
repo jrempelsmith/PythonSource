414f07f3eb939fb2ba951478026258ed0141477e
jrempelsmith <145237815+jrempelsmith@users.noreply.github.com>
2024-02-27 15:02:18 -0600

    My VS changes

--- Source1.py
+++ Source1.py
@@ -3,8 +3,11 @@ for i in range(5):
     print(str(i) * i)
     if i < 2:
         print(str(i) + " is neither prime nor composite.")
+    if i > 0 and i < 4:
+        print(str(i) + " is primooo.")
     else:
         print(str(i) + " is composite.")
+
 
         
 
