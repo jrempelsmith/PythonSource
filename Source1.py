8e2605e05ab0f5878a945de85424b6d8d6b8212b
jrempelsmith <145237815+jrempelsmith@users.noreply.github.com>
2024-02-27 14:58:54 -0600

    Updated it so it's better now.

--- Source1.py
+++ Source1.py
@@ -1,11 +1,10 @@
 print("hello")
 for i in range(5):
     print(str(i) * i)
-    if i == < 2:
+    if i < 2:
         print(str(i) + " is neither prime nor composite.")
-    elif i < 4:
-        print(str(i) + " is prime.")
     else:
         print(str(i) + " is composite.")
+
         
 
