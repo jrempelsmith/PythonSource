a569acc605f9bbe1e73fcdd6cba2328b733259cb
jrempelsmith <145237815+jrempelsmith@users.noreply.github.com>
2024-02-27 15:01:30 -0600

    Update Source1.py

--- Source1.py
+++ Source1.py
@@ -3,6 +3,8 @@ for i in range(5):
     print(str(i) * i)
     if i < 2:
         print(str(i) + " is neither prime nor composite.")
+    elif i < 4:
+        print(str(i) + " is prime.")
     else:
         print(str(i) + " is composite.")
 
